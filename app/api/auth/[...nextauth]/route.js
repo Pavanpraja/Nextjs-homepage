382d0f411c7b1f3c677cbd2c45dd385cd23f734f
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-07-13 12:43:35 +0530

    some changes again

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
-import NextAuth from 'next-auth'
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler =  NextAuth({
+const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -10,11 +10,27 @@ const handler =  NextAuth({
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    })
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+      callbacks: {
+        async signIn({ account, profile }) {
+          if (account.provider === "google") {
+            return (
+              profile.email_verified && profile.email.endsWith("@gmail.com")
+            );
+          }
+          return true;
+        },
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+});
 
-export {handler as GET, handler as POST}
-
+export { handler as GET, handler as POST };
